d601c8ab33f9b13cc7414e52ead0ba1f3bec7ec9
stonefeld <theo.stanfield@gmail.com>
2024-11-15 12:47:22 -0300

    Queries 1 y 2 funcionando

--- src/index.js
+++ src/index.js
@@ -1,72 +1,73 @@
 import { MongoClient } from "mongodb";
-import { createClient } from 'redis';
+import { createClient } from "redis";
 
-// Replace the uri string with your connection string.
-const url = "mongodb://localhost:27017";
-const mongo = new MongoClient(url);
+async function setClients() {
+  const mongo = new MongoClient("mongodb://localhost:27017");
+  const redis = createClient();
 
-// Redis Connection
-const redis = createClient();
+  await mongo.connect().catch((err) => console.log("Mongo Client Error", err));
 
-// 1. Obtener los datos de los clientes junto con sus teléfonos. 
+  redis.on("error", (err) => console.log("Redis Client Error", err));
+  await redis.connect();
+
+  return { mongo, redis };
+}
+
+// 1. Obtener los datos de los clientes junto con sus teléfonos.
 async function clientAndCellphones() {
-  try {
-    await mongo.connect().catch(err => console.log('Mongo Client Error', err));
-    const database = mongo.db('db2');
-    const clients = database.collection('clients');
+  const { mongo, redis } = await setClients();
 
-    const result = await clients.find({}).toArray();
+  try {
+    const database = mongo.db("db2");
+    const clients = database.collection("clients");
+
+    const result = await clients.find({}, { projection: { _id: 0 } }).toArray();
 
     for (const client of result) {
-      const data = JSON.stringify(client);
-      await redis.set(`clients:${data.nro_cliente}`, data);
+      await redis.hSet(`clients:${client.nro_cliente}`, client);
     }
 
     // TODO: Revisar si agregamos al set clients:names:<nombre> = id
 
     console.log(result);
-
   } finally {
     await mongo.close();
+    await redis.quit();
   }
 }
 
-clientAndCellphones().catch(console.dir);
-
 // 2. Obtener el/los teléfono/s y el número de cliente del cliente con nombre “Jacob” y apellido “Cooper”.
 async function findJacobCooper() {
+  const { mongo, redis } = await setClients();
+
   try {
-    // Busco en Redis
-    redis.connect().catch(err => console.log('Redis Client Error', err));
+    let clientID = await redis.get("clients:names:JacobCooper");
 
-    let clientID = await redis.get('clients:names:JacobCooper');
-    
     if (!clientID) {
-      await mongo.connect().catch(err => console.log('Mongo Client Error', err));
-      const database = mongo.db('db2');
-      const clients = database.collection('clients');
+      const database = mongo.db("db2");
+      const clients = database.collection("clients");
 
-      const projection = { _id: 1 };
-      const result = await clients.findOne({ nombre: 'Jacob', apellido: 'Cooper' }, { projection });
+      const projection = { nro_cliente: 1 };
+      const result = await clients.findOne(
+        { nombre: "Jacob", apellido: "Cooper" },
+        { projection }
+      );
       if (result) {
-        clientID = result._id;
-        await redis.set('clients:names:JacobCooper', clientID);
+        clientID = result.nro_cliente;
+        await redis.set("clients:names:JacobCooper", clientID);
       } else {
-        console.log('No se encontró el cliente');
+        console.log("No se encontró el cliente");
         return;
       }
     }
 
-    let client = await redis.get(`clients:${clientID}`);
-    data = JSON.stringify(client);
-     result = { _id: data.nro_cliente, telefonos: data.telefonos };
-     
-     console.log(result);
-    
+    const client = await redis.hGetAll(`clients:${clientID}`);
+    console.log({ _id: client.nro_cliente, telefonos: client.telefonos });
   } finally {
     await mongo.close();
     await redis.quit();
-  }  
+  }
 }
 
+clientAndCellphones().catch(console.dir);
 findJacobCooper().catch(console.dir);
