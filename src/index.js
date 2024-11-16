a80bce95324c375a1e8804cb2a596a5ffee110d1
PakiQuian <f.quianblanco@gmail.com>
2024-11-16 17:42:33 -0300

    Query 5 y 7

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import {
   generateFacturas,
   generateClientes,
   generateProductos,
-  insertClientRedis,
 } from "./data.js";
 
 // 0. Cargar los datos a la base de datos.
@@ -116,7 +115,7 @@ async function phonesAndClientData() {
         },
         {
           $project: {
-            _id: 0, // sacamos el id
+            _id: 0,
             codigo_area: "$telefonos.codigo_area",
             nro_telefono: "$telefonos.nro_telefono",
             tipo: "$telefonos.tipo",
@@ -134,6 +133,7 @@ async function phonesAndClientData() {
     console.error("Error ejecutando la consulta:", err);
   } finally {
     await mongo.close();
+    await redis.quit();
   }
 }
 
@@ -182,7 +182,42 @@ async function clientsWithBills() {
 
 // 5. Identificar todos los clientes que no tengan registrada ninguna factura.
 async function clientsWithoutBills() {
+  const { mongo, redis } = await setDbClients();
+  try {
+    const database = mongo.db("db2");
+    const clientes = database.collection("clientes");
 
+    const result = await clientes.aggregate([
+      {
+        $lookup: {
+          from: "facturas",
+          localField: "nro_cliente",
+          foreignField: "nro_cliente",
+          as: "facturas",
+        },
+      },
+      {
+        $match: {
+          "facturas.0": { $exists: false },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          nombre: 1,
+          apellido: 1,
+          nro_cliente: 1,
+        },
+      },
+    ]).toArray();
+
+    console.log(result);
+  } catch (err) {
+    console.error("Error ejecutando la consulta:", err);
+  } finally {
+    await mongo.close();
+    await redis.quit();
+  }
 }
 
 // 6. Devolver todos los clientes, con la cantidad de facturas que tienen registradas (si no tienen considerar cantidad en 0)
@@ -196,10 +231,10 @@ async function clientsWithBillsCount() {
     const result = await clientes.aggregate([
       {
         $lookup: {
-          from: "facturas", // Colección con la que se realiza el join
-          localField: "nro_cliente", // Campo de la colección clientes
-          foreignField: "nro_cliente", // Campo de la colección facturas
-          as: "facturas" // El alias para la información relacionada con facturas
+          from: "facturas",
+          localField: "nro_cliente",
+          foreignField: "nro_cliente",
+          as: "facturas"
         }
       },
       {
@@ -208,7 +243,7 @@ async function clientsWithBillsCount() {
           nombre: 1,
           apellido: 1,
           nro_cliente: 1,
-          facturas_count: { $ifNull: [{ $size: "$facturas" }, 0] } // Contamos las facturas, y si no tiene, asignamos 0
+          facturas_count: { $ifNull: [{ $size: "$facturas" }, 0] }
         }
       }
     ]).toArray();
@@ -223,10 +258,48 @@ async function clientsWithBillsCount() {
   }
 }
 
-
-// 7. Listar los datos de todas las facturas que hayan sido compradas por el cliente de nombre"Kai" y apellido "Bullock".
+// 7. Listar los datos de todas las facturas que hayan sido compradas por el cliente de nombre "Kai" y apellido "Bullock".
 async function findKaiBullockBills() {
+  const { mongo, redis } = await setDbClients();
 
+  try {
+    const database = mongo.db("db2");
+    const clientes = database.collection("clientes");
+    const facturas = database.collection("facturas");
+
+    const client = await redis.get("clientes:names:KaiBullock");
+
+    if (!client) {
+      const result = await clientes.findOne(
+        { nombre: "Kai", apellido: "Bullock" },
+        { projection: { _id: 0 } },
+      );
+
+      if (result) {
+        const clientID = result.nro_cliente;
+
+        const keyId = `clientes:names:${result.nombre}${result.apellido}`;
+        const keyClient = `clientes:${result.nro_cliente}`;
+
+        await redis.set(keyId, clientID);
+        await redis.set(keyClient, JSON.stringify(result));
+      
+        client = clientID;
+      } else {
+        console.log("Cliente no encontrado");
+        return;
+      }
+    }
+
+    const result = await facturas
+      .find({ nro_cliente: Number(client) }, { projection: { _id: 0 } })
+      .toArray();
+
+    console.log(JSON.stringify(result, null, 2));
+  } finally {
+    await mongo.close();
+    await redis.quit();
+  }
 }
 
 // 8. Seleccionar los productos que han sido facturados al menos 1 vez.
@@ -276,16 +349,16 @@ async function updateProduct(id) {
 
 }
 
-await loadData().catch(console.dir);
-await clientAndCellphones().catch(console.dir);
-await findJacobCooper().catch(console.dir);
-await phonesAndClientData().catch(console.dir);
-await clientsWithBills().catch(console.dir);
-await clientsWithoutBills().catch(console.dir);
-await clientsWithBillsCount().catch(console.dir);
+// await loadData().catch(console.dir);
+// await clientAndCellphones().catch(console.dir);
+// await findJacobCooper().catch(console.dir);
+// await phonesAndClientData().catch(console.dir);
+// await clientsWithBills().catch(console.dir);
+// await clientsWithoutBills().catch(console.dir);
+// await clientsWithBillsCount().catch(console.dir);
 await findKaiBullockBills().catch(console.dir);
-await productsWithBills().catch(console.dir);
-await billsWithIpsumProducts().catch(console.dir);
-await clientsWithTotalSpent().catch(console.dir);
-await billsOrderedByDateView().catch(console.dir);
-await productsNotBilledView().catch(console.dir);
+// await productsWithBills().catch(console.dir);
+// await billsWithIpsumProducts().catch(console.dir);
+// await clientsWithTotalSpent().catch(console.dir);
+// await billsOrderedByDateView().catch(console.dir);
+// await productsNotBilledView().catch(console.dir);
